14026a0e57547cfbb33a528bac010a4410123724
Ryan Raczak <ryanraczak@rocketmortgage.com>
2021-11-01 17:38:02 -0400

    initial commit

---
+++ PigLatinBuggy-main/PigLatin/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace PigLatin
+{
+    public class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string userInput = GetInput("Please input a word or sentence to translate to pig Latin");
+
+        //    string translation = ToPigLatin(userInput);
+        //    Console.WriteLine(translation);
+        //}
+
+        public static string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine().ToLower().Trim();
+            return input;
+        }
+
+        public static bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            foreach (char vowel in vowels)
+            {
+                if (c == vowel)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static string ToPigLatin(string word)
+        {
+            //Modified the program to take in a word or split
+            //a string with several words into a string array.
+            //It will then cycle the process until all words have
+            //gone through the translator
+
+            //Added various bools and additional string variables
+            //to handle the extra information being passed from 
+            //an array as well as shifting certain variables/bools 
+            //around to deal with an array. 
+
+            //Outside of the one string of several loops, there was
+            //logic errors in IsVowel that wasn't having it produce
+            //correct results, Runtime errors due to incorrect indexing,
+            //and leaving the 'w' out of 'way' when dealing with words
+            //that started with a vowel. 
+            string output = "";
+            char[] specialChars = { '@', '.', '-', '$', '^', '&' };
+            word = word.ToLower();
+            string[] words = word.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                bool specialCaught = false;
+                foreach (char c in specialChars)
+                {
+                    foreach (char w in words[i])
+                    {
+                        if (w == c)
+                        {
+                            if (!specialCaught)
+                            {
+                                specialCaught = true;
+                                Console.WriteLine("That word has special characters, we will return it as is");
+                                output += words[i];
+                                break;
+                            }
+
+                        }
+                    }
+
+                }
+
+                bool noVowels = true;
+                foreach (char letter in words[i])
+                {
+                    if (IsVowel(letter))
+                    {
+                        noVowels = false;
+                    }
+                }
+
+                if (noVowels)
+                {
+                    output += words[i];
+                }
+
+                string modOutput = "";
+                char firstLetter = words[i][0];
+                if (IsVowel(firstLetter) == true)
+                {
+                    modOutput = words[i] + "way";
+                }
+                else if (!specialCaught && !noVowels)
+                {
+                    int vowelIndex = -1;
+                    //Handle going through all the consonants
+                    string wordCut = words[i];
+                    for (int j = 0; j <= wordCut.Length; j++)
+                    {
+                        if (IsVowel(wordCut[j]) == true)
+                        {
+                            vowelIndex = j;
+                            break;
+                        }
+                    }
+
+                    string sub = words[i].Substring(vowelIndex);
+                    string postFix = words[i].Substring(0, vowelIndex);
+
+                    modOutput += sub + postFix + "ay";
+                }
+                output += modOutput + " ";
+            }
+            return output.Trim();
+        }
+    }
+}
---
+++ PigLatinBuggy-main/PigLatin/TestProgram.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PigLatin
+{
+    public class TestingProgram
+    {
+        [Theory]
+        [InlineData("apple")]
+        [InlineData("heck")]
+        [InlineData("strong")]
+        [InlineData("tommy@email.com")]
+        [InlineData("aardvark")]
+        [InlineData("Tommy")]
+        [InlineData("gym")]
+        [InlineData("apple joy gym tommy@email.com strong")]
+        public void TestingToPigLatin(string input)
+        {
+            //Arrange:
+            string expected = "";
+            string actual = "";
+
+
+            //Act:
+            switch (input)
+            {
+                case "apple":
+                    expected = "appleway";
+                    break;
+                case "heck":
+                    expected = "eckhay";
+                    break;
+                case "strong":
+                    expected = "ongstray";
+                    break;
+                case "tommy@email.com":
+                    expected = "tommy@email.com";
+                    break;
+                case "aardvark":
+                    expected = "aardvarkway";
+                    break;
+                case "Tommy":
+                    expected = "ommytay";
+                    break;
+                case "gym":
+                    expected = "gym";
+                    break;
+                case "apple joy gym tommy@email.com strong":
+                    expected = "appleway oyjay gym tommy@email.com ongstray";
+                    break;
+                default:
+                    break;
+            }
+
+            actual = Program.ToPigLatin(input);
+
+            //Assert:
+            Assert.Equal(expected, actual);
+        }
+    }
+}
